fe2947210f6d7fd8d909ca04e48414d2f7bc0075
silmemobile104 <silmemobile104@gmail.com>
2026-06-05 17:53:20 +0700

    Update all database.php files to use DB_PORT for Aiven MySQL

--- admin/database.php
+++ admin/database.php
@@ -5,10 +5,11 @@ $serverName = getenv('DB_HOST') ?: "127.0.0.1";
 $userName = getenv('DB_USER') ?: "silminmo_sg";
 $userPassword = getenv('DB_PASS') ?: "F5HqyW0iTSbx6DjU";
 $dbName = getenv('DB_NAME') ?: "silminmo_sg"; 
+$port = getenv('DB_PORT') ?: 3306;
 
-$objCon = mysqli_connect($serverName, $userName, $userPassword, $dbName);
-$con = mysqli_connect($serverName, $userName, $userPassword, $dbName);
-$conn = mysqli_connect($serverName, $userName, $userPassword, $dbName);
+$objCon = mysqli_connect($serverName, $userName, $userPassword, $dbName, $port);
+$con = mysqli_connect($serverName, $userName, $userPassword, $dbName, $port);
+$conn = mysqli_connect($serverName, $userName, $userPassword, $dbName, $port);
 
 mysqli_set_charset($objCon, "utf8");
 mysqli_set_charset($con, "utf8");
